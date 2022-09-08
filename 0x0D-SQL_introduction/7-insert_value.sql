3117f7e10bdf83bc29e976aa59b4d466ee1bedc6
ogbumah <ogbumalucky@gmail.com>
2022-09-08 21:32:11 +0100

    commit

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- add new row
--- add new row
-INSERT INTO first_table (id,name) VALUES (89,'Holberton School');
+-- inserts new record to first_table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
